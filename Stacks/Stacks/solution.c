1cf301469644d2b86918b4be42c66c63656d633c
AlfinST <alfinsthomas@gmail.com>
2018-04-30 00:27:27 +0530

    Stacks done

--- Stacks/Stacks/solution.c
+++ Stacks/Stacks/solution.c
@@ -2,5 +2,24 @@
 
 void main()
 {
-    int 
+    int f,i,N,cus[100000],top=-1;
+    scanf("%d",&N);
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&f);
+        if(f==1)
+        {
+            if(top<0)
+            printf("No food\n");
+            else
+            {
+                printf("%d\n",cus[top]);
+                top--;
+            }
+        }
+        else
+        {
+            scanf("%d",&cus[++top]);
+        }
+    }
 }
