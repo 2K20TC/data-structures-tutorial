6aacf9f5c0106db2dc1069c97de8d66a497c5996
RakeshPaiR <rakeshpair1998@gmail.com>
2018-05-02 01:56:39 +0530

    Heap solved

--- Trees/Heap/solution.c
+++ Trees/Heap/solution.c
@@ -1,96 +1,75 @@
 #include<stdio.h>
 
-void swap(int Arr[], int a, int b) {
-    
-    int temp = Arr[a];
-    Arr[a] = Arr[b];
-    Arr[b] = temp;
-    
+void swap(int arr[],int m,int n)
+{
+	int temp;
+	temp = arr[m];
+	arr[m] = arr[n];
+	arr[n] = temp;
 }
 
-void max_heapify (int Arr[], int i, int N) {
-    
-    int left = 2 * i;
-    int right = (2 * i) + 1;
-    int largest = i;
-    
-    if(left <= N && Arr[left] > Arr[i] ) {
-        
-        largest = left;
-        
-    }
-    else {
-        
-        largest = i;
-        
-    }
-    
-    if(right <= N && Arr[right] > Arr[largest]) {
-        
-        largest = right;
-        
-    }
-    if(largest != i) {
-        
-        swap(Arr,largest,i);
-        max_heapify(Arr,largest,N);
-        
-    }
-    
-}
-void buildMaxHeap(int Arr[],int N) {
-    
-    int i;
-    for(i = N/2; i >= 1; i--) {
-        
-        max_heapify(Arr,i,N);
-        
-    }
+void heapify(int arr[],int n,int i)
+{
+	int max = i;
+	int left = (2*i)+1;
+	int right = (2*i)+2;
+	
+	if(left<n && arr[left]>arr[max])
+		max = left;
+	if(right<n && arr[right]>arr[max])
+		max = right;
+	
+	if(max!=i)
+	{
+		swap(arr,max,i);
+		heapify(arr,n,max);
+	}
 }
 
-int insert(int Arr[], int N,int val) {
-    
-    int i;
-    N++;
-    i = N;
-    Arr[ i ] = val;
-        while( i > 1 && Arr[ i/2 ] < Arr[ i ])
-        {
-            swap(Arr,i/2, i );
-            i = i/2;
-        }
-    return N;
-    
+void heapsort(int arr[],int n)
+{
+	int i;
+	for(i=n/2 -1;i>=0;i--)
+		heapify(arr,n,i);
+	for(i=n-1;i>=0;i--)
+	{
+		swap(arr,0,i);
+		heapify(arr,i,0);
+	}
 }
-main() {
-    
-    int N,i,q,t;
-    unsigned long long Arr[100001],c;
-    
-    scanf("%d",&N);
-    for(i = 1; i <= N; i++) {
-        
-        scanf("%llu",&Arr[i]);
-        
-    }
-    buildMaxHeap(Arr,N);
-    scanf("%d",&q);
-    do {
-        
-        scanf("%d",&t);
-        if(t == 1) {
-            
-            scanf("%d",&c);
-            
-            N = insert(Arr,N,c);
-            
-        }
-        else {
-            
-            printf("%d",Arr[1]);
-            
-        }
-        
-        q--;
-    }while(q > 0);
+
+/*void output(int arr[],int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+		printf("%d\t",arr[i]);
+}*/
+
+int main()
+{
+	int i,n,k,arr[100000],q;
+		//printf("Enter array elements: ");
+		scanf("%d",&n);
+		for(i=0;i<n;i++)
+		{
+			//printf("\narr[%d]: ",i);
+			scanf("%d",&arr[i]);
+		}
+		heapsort(arr,n);
+		scanf("%d",&q);
+		do {
+		scanf("%d",&i);
+		    if( i == 1) {
+
+		        scanf("%d",&arr[n++]);
+		        heapsort(arr,n);
+
+		    }
+		    else if( i == 2 ) {
+		        
+		        printf("%d\n",arr[n-1]);
+
+		    }
+		    q--;
+	    }while(q > 0);
 }
