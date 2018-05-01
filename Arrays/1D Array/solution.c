e12f4c03d4ba638e252fb95d4bcc5777a7b6c7e3
Akhil Seshan <akhilses@gmail.com>
2018-05-01 22:41:30 +0530

    solution of 1D array

--- Arrays/1D Array/solution.c
+++ Arrays/1D Array/solution.c
@@ -1,12 +1,15 @@
+
 #include<stdio.h>
+#include<stdlib.h>
 void main()
 {
-  int N,i,*A;
-  scanf("%d",N);
+  int N,i;
+  int *A;
+  scanf("%d",&N);
   A=(int *)malloc(N*sizeof(int));
   for(i=0;i<N;i++)
     scanf("%d",&A[i]);
-   for(i=N-1;i>=0;i--)
+  for(i=N-1;i>=0;i--)
     printf("%d\n",A[i]);
-   free(A);
+  free(A);
 }
